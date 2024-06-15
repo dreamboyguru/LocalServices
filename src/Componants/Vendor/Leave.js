879a0710edd001afc2cfcf575a82c1853ee4edd5
dreamboyguru <guruhanamannavar870@gmail.com>
2024-06-15 22:55:34 +0530

    correction slot and backend leave api

--- src/Componants/Vendor/Leave.js
+++ src/Componants/Vendor/Leave.js
@@ -1,23 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+
+const validationSchema = Yup.object({
+    date: Yup.date().required('Required'),
+});
+
 
 const Leave = () => {
-    const leave = [
-        {date : '01-06-2024', status : '1'},
-        {date : '01-06-2024', status : '2'}, 
-        {date : '01-06-2024', status : '0'},
-        {date : '01-06-2024', status : '0'}
-    ]
+    const [leaveData, setLeaveData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const email = localStorage.getItem('email');
+    const url = process.env.REACT_APP_API_URL;
+    
+    const initialValues = {
+        date: '',
+    };
+
+    useEffect(()=> {
+        const fetchData = async() => {
+            try {
+                const response = await axios.get(`${url}/api/leave/${email}`);
+                console.log(response.data);
+                setLeaveData(response.data);
+            } catch (err) {
+                console.log(err);
+                setLeaveData([
+                    {date : '01-06-2024', status : '1'},
+                    {date : '01-06-2024', status : '2'}, 
+                    {date : '01-06-2024', status : '0'},
+                    {date : '01-06-2024', status : '0'}
+                ])
+            } finally {
+                setLoading(false);
+            }
+        } 
+        fetchData()
+    }, [loading])
+
+    const handleSubmit = async (values, { setSubmitting }) => {
+        setLoading(true)
+        try {
+            const response = await axios.post(`${url}/api/leave`, {values, email});
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error submitting data:', error);
+        } finally {
+            setSubmitting(false);
+        }
+    };
   return (
     <div className="flex flex-wrap">
         <div className="bg-white rounded-md shadow-lg p-4 px-10 max-md:p-2 w-[40%] max-md:w-full mb-5 max-md:text-sm h-40 max-md:h-auto">
             <h1 className="text-2xl max-md:text-lg font-bold text-center text-gray-800 mb-6 max-md:mb-2">Select Leave Applying Date</h1>
             <div className='flex flex-row'>
-                <div className="flex justify-center w-full">
-                    <input type="date" className="w-full text-2xl max-md:text-sm text-black text-center border border-gray-300 rounded-l-lg p-2 max-md:p-1 focus:outline-none focus:ring-1 focus:ring-gray-100" />
-                </div>
-                <div className='w-1/3'>
-                    <button type='submit' className='p-3 max-md:p-1 px-4 bg-gray-500 text-2xl max-md:text-lg hover:bg-gray-600 w-full rounded-r-md duration-300'>Submit</button>
-                </div>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ isSubmitting }) => (
+                        <Form className="flex items-center justify-center w-full space-x-2">
+                            <div className="flex justify-center w-full">
+                                <Field
+                                    type="date"
+                                    name="date"
+                                    className="w-full text-2xl max-md:text-sm text-black text-center border border-gray-300 rounded-l-lg p-2 max-md:p-1 focus:outline-none focus:ring-1 focus:ring-gray-100"
+                                />
+                                <ErrorMessage name="date" component="div" className="text-red-500 text-sm mt-1" />
+                            </div>
+                            <div className='w-1/3'>
+                                <button
+                                    type="submit"
+                                    className='p-3 max-md:p-1 px-4 bg-gray-500 text-2xl max-md:text-lg hover:bg-gray-600 w-full rounded-r-md duration-300'
+                                    disabled={isSubmitting}
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </div>
         <div className="shadow-lg w-[58%] max-md:w-full mb-5 ml-5 max-md:ml-0 h-auto">
@@ -30,13 +95,13 @@ const Leave = () => {
                     </tr>
                 </thead>
                 <tbody className="text-center text-black">
-                    {leave && leave.map((item, index) =>
+                    {leaveData && leaveData.map((item, index) =>
                         <tr className="odd:bg-white even:bg-gray-50" key={item.id}>
                             <td className="border border-gray-300 px-4 py-4">{index+1}</td>
-                            <td className="border border-gray-300 px-4 py-4">{item.date}</td>
-                            {item.status === '0' ? 
+                            <td className="border border-gray-300 px-4 py-4">{item?.date.split('T')[0]}</td>
+                            {item.status === 0 ? 
                                 <td className="border border-gray-300 px-4 py-4 font-bold text-yellow-600">Processing</td> :
-                                item.status === '1' ?
+                                item.status === 1 ?
                                     <td className="border border-gray-300 px-4 py-4 font-bold text-green-600">Approved</td> :
                                     <td className="border border-gray-300 px-4 py-4 font-bold text-red-600">Rejected</td>
                             }
--- src/Componants/Vendor/Slots.js
+++ src/Componants/Vendor/Slots.js
@@ -11,17 +11,11 @@ const validationSchema = Yup.object({
 
 const Slots = () => {
     const [ratesData, setRatesData] = useState([]);
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoding] = useState(false);
     const email = localStorage.getItem('email')
     const url = process.env.REACT_APP_API_URL;
-    const initialValues = {
-        oneDay: '',
-        oneWeek: '',
-        oneMonth: '',
-    };
-
+    
     useEffect(()=>{
-        setLoading(true)
         const fetchData = async() => {
             try {
                 const response = await axios.get(`${url}/api/rates/${email}`);
@@ -30,16 +24,21 @@ const Slots = () => {
             }
             catch (err) {
                 console.log(err);
-            }
-            finally {
-                setLoading(false);
+            } finally {
+                setLoding(false)
             }
         } 
         fetchData()
-    }, [])
+    },[loading])
+
+    const initialValues = {
+        oneDay: ratesData.one_day || '',
+        oneWeek: ratesData.one_week || '',
+        oneMonth: ratesData.one_month || '',
+    };
 
     const handleSubmit = async (values, { setSubmitting }) => {
-        setLoading(true)
+        setLoding(true)
         try {
             const response = await axios.put(`${url}/api/rates`, {values, email});
             console.log(response.data);
@@ -47,22 +46,67 @@ const Slots = () => {
             console.error(error);
         } finally {
             setSubmitting(false);
-            setLoading(false);
         }
     };
 
     return (
         <div className='flex justify-center'>
-            { loading ? 
-                <div>loading...</div> :
-                <div className='min-h-96 text-2xl w-[50%] mt-20 max-md:w-full text-black overflow-x-auto'>
-                    <Formik
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={handleSubmit}
-                    >
-                        {({ isSubmitting }) => (
-                            <Form>
+            <div className='min-h-96 text-2xl w-[50%] mt-20 max-md:w-full text-black overflow-x-auto'>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            {(ratesData === null) || (ratesData?.one_day === 0) || (ratesData?.one_week === 0) || (ratesData?.one_month === 0)  ? 
+                                <>
+                                    <table className="min-w-full border-collapse border border-gray-300 shadow-lg text-lg">
+                                        <thead>
+                                            <tr className="bg-gray-500 text-white max-md:text-sm">
+                                                <th className="border border-gray-300 px-4 py-2 w-1/12 max-md:px-1">SL.No</th>
+                                                <th className="border border-gray-300 px-4 py-2 w-3/12 max-md:px-1">Duration</th>
+                                                <th className="border border-gray-300 px-4 py-2 w-4/12 max-md:px-1">Rates</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="text-center">
+                                            <tr className="odd:bg-white even:bg-gray-50 max-md:text-sm">
+                                                <td className="border border-gray-300 px-4 py-2">1</td>
+                                                <td className="border border-gray-300 px-4 py-2">One Day</td>
+                                                <td className="border border-gray-300 px-4 py-2">
+                                                    <Field type="number" name="oneDay" className="border border-gray-300 rounded p-2 w-full" />
+                                                    <ErrorMessage name="oneDay" component="div" className="text-red-500" />
+                                                </td>
+                                            </tr>
+                                            <tr className="odd:bg-white even:bg-gray-50 max-md:text-sm">
+                                                <td className="border border-gray-300 px-4 py-2">2</td>
+                                                <td className="border border-gray-300 px-4 py-2">One Week</td>
+                                                <td className="border border-gray-300 px-4 py-2">
+                                                    <Field type="number" name="oneWeek" className="border border-gray-300 rounded p-2 w-full" />
+                                                    <ErrorMessage name="oneWeek" component="div" className="text-red-500" />
+                                                </td>
+                                            </tr>
+                                            <tr className="odd:bg-white even:bg-gray-50 max-md:text-sm">
+                                                <td className="border border-gray-300 px-4 py-2">3</td>
+                                                <td className="border border-gray-300 px-4 py-2">One Month</td>
+                                                <td className="border border-gray-300 px-4 py-2">
+                                                    <Field type="number" name="oneMonth" className="border border-gray-300 rounded p-2 w-full" />
+                                                    <ErrorMessage name="oneMonth" component="div" className="text-red-500" />
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <div className="flex justify-center mt-4">
+                                        <button
+                                            type="submit"
+                                            className='bg-gray-500 hover:bg-gray-600 text-white items-end rounded-md p-2 px-8 max-md:mr-1 max-md:px-3 max-md:py-1 max-md:text-lg hover:scale-105 duration-300'
+                                            disabled={isSubmitting}
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                 </> :
+
                                 <table className="min-w-full border-collapse border border-gray-300 shadow-lg text-lg">
                                     <thead>
                                         <tr className="bg-gray-500 text-white max-md:text-sm">
@@ -76,57 +120,30 @@ const Slots = () => {
                                             <td className="border border-gray-300 px-4 py-2">1</td>
                                             <td className="border border-gray-300 px-4 py-2">One Day</td>
                                             <td className="border border-gray-300 px-4 py-2">
-                                                {ratesData.one_day === 0 ? 
-                                                    <>
-                                                        <Field type="number" name="oneDay" className="border border-gray-300 rounded p-2 w-full" />
-                                                        <ErrorMessage name="oneDay" component="div" className="text-red-500" />
-                                                    </> :
-                                                    <span className='w-full text-black'>{ratesData.one_day}</span>
-                                                }
+                                                {ratesData?.one_day}
                                             </td>
                                         </tr>
                                         <tr className="odd:bg-white even:bg-gray-50 max-md:text-sm">
                                             <td className="border border-gray-300 px-4 py-2">2</td>
                                             <td className="border border-gray-300 px-4 py-2">One Week</td>
                                             <td className="border border-gray-300 px-4 py-2">
-                                                {ratesData.one_week === 0 ? 
-                                                    <>
-                                                        <Field type="number" name="oneDay" className="border border-gray-300 rounded p-2 w-full" />
-                                                        <ErrorMessage name="oneDay" component="div" className="text-red-500" />
-                                                    </> :
-                                                    <span className='w-full text-black'>{ratesData.one_week}</span>
-                                                }
+                                                {ratesData?.one_week}
                                             </td>
                                         </tr>
                                         <tr className="odd:bg-white even:bg-gray-50 max-md:text-sm">
                                             <td className="border border-gray-300 px-4 py-2">3</td>
                                             <td className="border border-gray-300 px-4 py-2">One Month</td>
                                             <td className="border border-gray-300 px-4 py-2">
-                                                {ratesData.one_month === 0 ? 
-                                                    <>
-                                                        <Field type="number" name="oneMonth" className="border border-gray-300 rounded p-2 w-full" />
-                                                        <ErrorMessage name="oneMonth" component="div" className="text-red-500" />
-                                                    </> :
-                                                    <span className='w-full text-black'>{ratesData.one_month}</span>
-                                                }
+                                                {ratesData?.one_month}
                                             </td>
                                         </tr>
                                     </tbody>
                                 </table>
-                                <div className="flex justify-center mt-4">
-                                    <button
-                                        type="submit"
-                                        className='bg-gray-500 hover:bg-gray-600 text-white items-end rounded-md p-2 px-8 max-md:mr-1 max-md:px-3 max-md:py-1 max-md:text-lg hover:scale-105 duration-300'
-                                        disabled={isSubmitting}
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </Form>
-                        )}
-                    </Formik>
-                </div>
-            }
+                            }
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </div>
     );
 };
